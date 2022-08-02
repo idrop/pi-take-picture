2c41651cd0f83ee355a20e1d780cf2fb4c47c2a0
Phil Miller <phil@skabenga.com>
2022-08-01 21:31:00 -0400

    Save to a dir off home

--- picture.py
+++ picture.py
@@ -1,17 +1,24 @@
 #!//usr/bin/python3
 
 import time
+from pathlib import Path
 
 from picamera2 import Picamera2
 
-picam2 = Picamera2()
-config = picam2.create_still_configuration()
-picam2.configure(config)
 
-picam2.start()
-time.sleep(5)
-now = time.strftime("%Y%m%d-%H%M%S", time.gmtime())
-np_array = picam2.capture_array()
-print(np_array)
-picam2.capture_file(now + ".jpg")
-picam2.stop()
+def take_pic():
+    picam2 = Picamera2()
+    config = picam2.create_still_configuration()
+    picam2.configure(config)
+    picam2.start()
+    time.sleep(5)
+    now = time.strftime("%Y%m%d-%H%M%S", time.gmtime())
+    np_array = picam2.capture_array()
+    print(np_array)
+
+    home_dir = str(Path.home())
+    picam2.capture_file(home_dir + "/current/" + now + ".jpg")
+    picam2.stop()
+
+
+take_pic()
